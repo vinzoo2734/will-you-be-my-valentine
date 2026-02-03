231fc7001960aff8e6ec69500926ab5682673114
Vinay Mahale <119444352+vinzoo2734@users.noreply.github.com>
2026-02-03 22:46:57 +0530

    Update to date invitation: remove names, simplify to English-only, change question to date request

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 
 # Contributions
 - A french & thai version of the above have been added to the site. Use the language selecter :D
-- All thanks to [YidirK](https://github.com/YidirK) for the french translation.
-- All thanks to [RayeMilk](https://github.com/RayeMilk) for the thai translation.
+- Thanks to the community for the french translation.
+- Thanks to the community for the thai translation.
--- public/js/script.js
+++ public/js/script.js
@@ -1,64 +1,23 @@
-const answers_no = {
-    english: [
-        "No",
-        "Are you sure?",
-        "Are you really sure??",
-        "Are you really realy sure???",
-        "Think again?",
-        "Don't believe in second chances?",
-        "Why are you being so cold?",
-        "Maybe we can talk about it?",
-        "I am not going to ask again!",
-        "Ok now this is hurting my feelings!",
-        "You are now just being mean!",
-        "Why are you doing this to me?",
-        "Please give me a chance!",
-        "I am begging you to stop!",
-        "Ok, Let's just start over.."
-    ],
-    french: [
-        "Non",
-        "Tu es sûr ?",
-        "Tu es vraiment sûr ??",
-        "Tu es vraiment vraiment sûr ???",
-        "Réfléchis encore?",
-        "Tu ne crois pas aux deuxièmes chances ?",
-        "Pourquoi tu es si froid?",
-        "Peut-être, on peut en parler ?",
-        "Je ne vais pas demander encore une fois!",
-        "D'accord, maintenant ca me fait mal!",
-        "Tu es juste méchant!",
-        "Pourquoi tu me fais ça?",
-        "Donnez-moi une chance plz!",
-        "Je te supplie d'arrêter!",
-        "D'accord, recommençons.."
-    ],
-    thai: [
-        "ไม่อ่ะ",
-        "แน่ใจจริงๆหรอคะ?",
-        "แน่ใจจริงๆ จริงๆนะคะ?",
-        "อย่าบอกนะว่านี่แน่ใจสุดๆแล้วจริงๆ ?",
-        "ลองคิดดูอีกทีหน่อยสิคะ..",
-        "ขอโอกาศที่สองทีค่ะ..",
-        "อย่าเย็นชาสิคะ กระซิกๆ",
-        "ขอร้องนะคะ",
-        "น้าาาๆๆๆๆๆ",
-        "เราจะร้องไห้เอานะ กระซิกๆ",
-        "จะเอางี้ๆจริงหรอคะ",
-        "ฮือออออ",
-        "ขอโอกาศครั้งที่สองที่ค่ะ!",
-        "ขอร้องละค่าาา",
-        "โอเคค่ะ.. งั้นเริ่มใหม่ !"
-    ]
-};
+const answers_no = [
+    "No",
+    "Are you sure?",
+    "Are you really sure??",
+    "Are you really realy sure???",
+    "Think again?",
+    "Don't believe in second chances?",
+    "Why are you being so cold?",
+    "Maybe we can talk about it?",
+    "I am not going to ask again!",
+    "Ok now this is hurting my feelings!",
+    "You are now just being mean!",
+    "Why are you doing this to me?",
+    "Please give me a chance!",
+    "I am begging you to stop!",
+    "Ok, Let's just start over.."
+];
 
-answers_yes = {
-    "english": "Yes",
-    "french": "Oui",
-    "Thailand": "เย่ คืนดีกันแล้วน้า"
-}
+const answers_yes = "Yes";
 
-let language = "english"; // Default language is English
 const no_button = document.getElementById('no-button');
 const yes_button = document.getElementById('yes-button');
 let i = 1;
@@ -79,16 +38,16 @@ no_button.addEventListener('click', () => {
     size += sizes[random]
     yes_button.style.height = `${size}px`;
     yes_button.style.width = `${size}px`;
-    let total = answers_no[language].length;
+    let total = answers_no.length;
     // change button text
     if (i < total - 1) {
-        no_button.innerHTML = answers_no[language][i];
+        no_button.innerHTML = answers_no[i];
         i++;
     } else if (i === total - 1) {
-        alert(answers_no[language][i]);
+        alert(answers_no[i]);
         i = 1;
-        no_button.innerHTML = answers_no[language][0];
-        yes_button.innerHTML = answers_yes[language];
+        no_button.innerHTML = answers_no[0];
+        yes_button.innerHTML = answers_yes;
         yes_button.style.height = "50px";
         yes_button.style.width = "50px";
         size = 50;
@@ -114,40 +73,4 @@ function refreshBanner() {
     let src = banner.src;
     banner.src = '';
     banner.src = src;
-}
-
-function changeLanguage() {
-    const selectElement = document.getElementById("language-select");
-    const selectedLanguage = selectElement.value;
-    language = selectedLanguage;
-
-    // Update question heading
-    const questionHeading = document.getElementById("question-heading");
-    if (language === "french") {
-        questionHeading.textContent = "Tu veux être mon valentin?";
-    } else if (language === "thai") {
-        questionHeading.textContent = "คืนดีกับเราได้อ่ะป่าว?";
-    } else {
-        questionHeading.textContent = "Will you be my valentine?";
-    }
-
-    // Reset yes button text
-    yes_button.innerHTML = answers_yes[language];
-
-    // Reset button text to first in the new language
-    if (clicks === 0) {
-        no_button.innerHTML = answers_no[language][0];
-    } else {
-        no_button.innerHTML = answers_no[language][clicks];
-    }
-
-    // Update success message
-    const successMessage = document.getElementById("success-message");
-    if (language === "french") {
-        successMessage.textContent = "Yepppie, à bientôt :3";
-    } else if (language === "thai") {
-        successMessage.textContent = "ฮูเร่ คืนดีกันแล้วน้า :3";
-    } else {
-        successMessage.textContent = "Yepppie, see you sooonnn :3";
-    }
 }
